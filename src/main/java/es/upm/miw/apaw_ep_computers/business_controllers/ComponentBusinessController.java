33f0002efa5e90246388b4fe9c618c7bf65e8e65
Alejandro <alem2518@hotmail.com>
2019-10-16 22:55:50 +0200

    add Delete /V0/Components/{id} #5

--- src/main/java/es/upm/miw/apaw_ep_computers/business_controllers/ComponentBusinessController.java
+++ src/main/java/es/upm/miw/apaw_ep_computers/business_controllers/ComponentBusinessController.java
@@ -4,6 +4,7 @@ import es.upm.miw.apaw_ep_computers.daos.ComponentDao;
 import es.upm.miw.apaw_ep_computers.documents.Component;
 import es.upm.miw.apaw_ep_computers.dtos.ComponentDto;
 
+import es.upm.miw.apaw_ep_computers.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -26,15 +27,19 @@ public class ComponentBusinessController {
         return new ComponentDto(component);
     }
 
-    public List<ComponentDto> readAll() {
-        List<Component> components = this.componentDao.findAll();
-        return components.stream().map(ComponentDto::new).collect(Collectors.toList());
-    }
-
     public List<ComponentDto> findByTypeEqual(String value) {
         return this.componentDao.findAll().stream()
                 .filter(component -> component.getType().equals(value))
                 .map(ComponentDto::new)
                 .collect(Collectors.toList());
+    }
+
+    public ComponentDto get(String id) {
+        Component component = this.componentDao.findById(id).orElseThrow(() -> new NotFoundException("Computer id: " + id));
+        return new ComponentDto(component);
+    }
+
+    public void delete(String id) {
+        this.componentDao.deleteById(id);
     }
 }
