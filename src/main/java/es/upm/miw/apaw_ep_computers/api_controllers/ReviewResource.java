b3053d79afc6622d92011cdeddd35415a789f726
Alejandro <alem2518@hotmail.com>
2019-10-20 21:30:49 +0200

    Add Get /v0/Review/Search?q=valuation:{valuation} #7

--- src/main/java/es/upm/miw/apaw_ep_computers/api_controllers/ReviewResource.java
+++ src/main/java/es/upm/miw/apaw_ep_computers/api_controllers/ReviewResource.java
@@ -2,17 +2,18 @@ package es.upm.miw.apaw_ep_computers.api_controllers;
 
 import es.upm.miw.apaw_ep_computers.business_controllers.ReviewBusinessController;
 import es.upm.miw.apaw_ep_computers.dtos.ReviewDto;
+import es.upm.miw.apaw_ep_computers.exceptions.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(ReviewResource.REVIEWS)
 public class ReviewResource {
 
     static final String REVIEWS = "/reviews";
+    static final String SEARCH = "/search";
 
     private ReviewBusinessController reviewBusinessController;
 
@@ -25,5 +26,13 @@ public class ReviewResource {
     public ReviewDto create(@RequestBody ReviewDto reviewDto) {
         reviewDto.validate();
         return this.reviewBusinessController.create(reviewDto);
+    }
+
+    @GetMapping(value = SEARCH)
+    public List<ReviewDto> find(@RequestParam String q) {
+        if (!"valuation".equals(q.split(":=")[0])) {
+            throw new BadRequestException("query param q is incorrect, missing 'valuation:='");
+        }
+        return this.reviewBusinessController.findByValuationEqual(q.split(":=")[1]);
     }
 }
